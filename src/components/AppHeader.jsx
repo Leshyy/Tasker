5baddf16b02a5416533317416d7a035b1afed0e2
Amit <amitwe26@gmail.com>
2021-01-12 22:47:22 +0200

    first day

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
+import logo from '../assets/styles/logo/logo.png'
 export function AppHeader() {
     return (
-        <div className="main-header">
-            <h1>logo</h1>
-            <h2>h2 in header</h2>
+        <div className="main-header flex space-between">
+            <div className="top-header flex ">
+                <img src={logo} alt="Logo" />
+                <span title="Noti">Noti</span>
+            </div>
+
+            <nav className="nav-header flex space-around">
+                <Link title="My week">My week</Link>
+                <Link title="My profile">P</Link>
+                <Link title="Logout">Logout</Link>
+            </nav>
         </div>
     )
 }
