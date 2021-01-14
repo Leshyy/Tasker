a5345f751bccdefb2c50563bc52f8b927ea380f1
Tair Bitan <tairbitan19@gmail.com>
2021-01-14 19:49:12 +0200

    style

--- src/cmps/board/BoardSideNav.jsx
+++ src/cmps/board/BoardSideNav.jsx
@@ -1,17 +1,24 @@
+import { render } from "@testing-library/react";
+import { Component } from "react";
 import { BoardFilter } from "./BoardFilter";
 import { BoardList } from "./BoardList";
 
-export function BoardSideNav({ boards, onRemove, onAdd, getBoradsForDisplay }) {
-    return (
-        <section className="board-side-nav flex">
-            <h2>My Boards:</h2>
-            <button onClick={() => {
-                onAdd('new board')
-            }}>add board</button>
-            <BoardFilter getBoradsForDisplay={getBoradsForDisplay} />
-            <BoardList boards={boards}
-                onRemove={onRemove}
-            />
-        </section>
-    )
+export class BoardSideNav extends Component {
+
+    render() {
+        const { boards, onRemove, onAdd, getBoradsForDisplay } = this.props;
+        return (
+            <section className="board-side-nav flex">
+                <div className="side-nav-toggle">&gt;</div>
+                <h2>My Boards:</h2>
+                <button onClick={() => {
+                    onAdd('new board')
+                }}>add board</button>
+                <BoardFilter getBoradsForDisplay={getBoradsForDisplay} />
+                <BoardList boards={boards}
+                    onRemove={onRemove}
+                />
+            </section>
+        )
+    }
 }
