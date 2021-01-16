978fc4fbd41d2168c3091da1de4efbcc0d5ca58c
Amit <amitwe26@gmail.com>
2021-01-16 19:07:57 +0200

    from amit

---
+++ src/cmps/tasks/columns/Members.jsx
@@ -0,0 +1,28 @@
+import { Avatar } from '@material-ui/core';
+import { AvatarGroup } from '@material-ui/lab';
+import Amit from '../../../assets/styles/img/Amit.jpeg';
+import Tamir from '../../../assets/styles/img/Tamir.jpeg';
+import Tair from '../../../assets/styles/img/Tair.jpeg';
+
+export function Members({ task }) {
+    // function getFirstName(fullName) {
+    //     const regex = fullName.replace(/\s.*$/, "").trim();
+    //     console.log('regex is:', regex);
+    //     return regex
+    // }
+    return (
+        <div>
+            <AvatarGroup max={3} className="avatar-container flex center" >
+                {/* {task.members.map((member) => {
+                    return <Avatar className="avatar" alt={`${member.fullname}`}
+                        src={getFirstName(member.fullname)} />
+
+                })} */}
+                {/* src={`../../../assets/styles/img/${getFirstName(member.fullname)}.jpeg`} */}
+                <Avatar className="avatar" alt="Amit" src={Amit} />
+                <Avatar className="avatar" alt="Amit" src={Tamir} />
+                <Avatar className="avatar" alt="Amit" src={Tair} />
+            </AvatarGroup>
+        </div>
+    )
+}
