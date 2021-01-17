31ee0de5b5a2c01ad4fdbf0e1e44af99a43a61b7
Tamir Leshetz <tamir.leshetz@gmail.com>
2021-01-17 14:40:24 +0200

    almost chat

---
+++ src/cmps/tasks/TaskChat.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react'
+
+export class TaskChat extends Component {
+    state = {
+        clickedInput: false
+    }
+    componentDidMount() {
+    }
+
+    toggleShowTextArea = () => {
+        this.setState({ clickedInput: !this.state.clickedInput })
+    }
+
+    render() {
+        const { clickedInput } = this.state
+        return (
+            <section className="task-chat" >
+                <div className="task-name-container">
+                    <span>{this.props.task.name}</span>
+                </div>
+                {!clickedInput &&
+                    <input type="text" onFocus={this.toggleShowTextArea} placeholder="write something..." />}
+                {clickedInput && <textarea onBlur={this.toggleShowTextArea} />}
+
+            </section>
+        )
+    }
+}
