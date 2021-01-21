508fca321bea0e64ff7137804af23f5c1b5a7832
Amit <amitwe26@gmail.com>
2021-01-21 23:10:27 +0200

    the end today cmps,react, tanks

--- frontend/src/cmps/DeleteModalGroup.jsx
+++ frontend/src/cmps/DeleteModalGroup.jsx
@@ -1,13 +1,13 @@
 
-export function DeleteModalTask({ task, group, onRemove, onCloseModalDelete }) {
+export function DeleteModalGroup({ group, onRemoveGroup, onCloseModalDelete }) {
 
     return (
         < div className="modal-delete flex col space-between" >
             <div className="top">
-                <p>You're sure want to delete Task? "{task.name}"</p>
+                <p>You're sure want to delete Group? "{group.name}"</p>
             </div>
             <div className="bottom flex align-center">
-                <button className="btn-delete" onClick={() => onRemove(task.id, group)}>Delete</button>
+                <button className="btn-delete" onClick={(ev) => onRemoveGroup(ev, group.id)}>Delete</button>
                 <button className="btn-go-back" onClick={onCloseModalDelete}>Go Back</button>
             </div>
         </div >
